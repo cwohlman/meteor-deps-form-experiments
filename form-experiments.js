fdbac971ac93e6c0cf8a73bc1087209e2bad936c
Joshua Ohlman <codewriterohs@gmail.com>
2014-09-16 23:31:02 -0500

    add items method

--- form-experiments.js
+++ form-experiments.js
@@ -4,15 +4,43 @@ Form = function (schema, value, parent) {
   this.value = value;
   this.parent = parent;
   this.fields = {};
+  this.children = {};
 };
 
 Form.prototype.field = function (fieldName) {
+  this.dep.depend();
+  var field = this.fields[fieldName] || new Form(this.schema[fieldName], this.value[fieldName], this);
   if (!this.fields[fieldName]) {
-    this.fields[fieldName] = new Form(this.schema[fieldName], this.value[fieldName], this);
+    this.fields[fieldName] = field;
+  } else {
+    field.value = this.value[fieldName];
   }
-  var field = this.fields[fieldName];
   field.dep.depend();
   return field;
+};
+
+Form.prototype.items = function (fieldName) {
+  var self = this;
+  this.childSchema = this.childSchema || new Schema({
+    name: this.name
+    , rules: this.rules
+    , schema: this.schema
+  });
+  this.dep.depend();
+  var children = {};
+  _.each(this.value, function (a, i) {
+    var key = a && a._id || i;
+    if (!self.children[key]) {
+      self.children[key] = new Form(self.childSchema, a, self);
+    } else {
+      self.children[key].value = a;
+    }
+    children[key] = self.children[key];
+  });
+  // cleanup deleted values
+  self.children = children;
+
+  return _.values(children);
 };
 
 Form.prototype.set = function (value) {
@@ -22,9 +50,6 @@ Form.prototype.set = function (value) {
   }
   this.value = value;
   this.dep.changed();
-  _.each(this.fields, function (a, i) {
-    a.set(value[i]);
-  });
 };
 
 var personSchema = new Schema({
@@ -36,7 +61,17 @@ var personSchema = new Schema({
   }
 });
 
-m = new Form(personSchema, {});
+m = new Form(personSchema, {
+  name: 'my name'
+  , neighbor: {
+    name: 'my neighbor'
+  }
+  , friends: [
+    {
+      name: 'sam'
+    }
+  ]
+});
 
 if (Meteor.isClient) {
   // counter starts at 0
@@ -49,6 +84,10 @@ if (Meteor.isClient) {
   Template.hello.events({
     'change input': function (e) {
       this.set(e.currentTarget.value);
+    }
+    , 'click button': function (e) {
+      this.value.push({name: 'new friend'});
+      this.set(this.value);
     }
   });
 }
